1e124e91d13514ed866d4ebc70b9195a849165c0
Tuğçe Ergün <tugceergun000@gmail.com>
2024-01-02 17:18:55 +0300

    update

--- FlashLanguage/app/(modals)/set/[id].tsx
+++ FlashLanguage/app/(modals)/set/[id].tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState} from 'react'
 import { useLocalSearchParams, useRouter } from 'expo-router'
 import {Set, getSet} from '@/data/api';
@@ -20,9 +20,14 @@ const Page = () => {
         loadSet();
     },[id]);
 
+    const onAddToFavorites = async() => {
+
+    }
+
   return (
     <View style={defaultStyleSheet.container}>
-    {set && <View style={{ alignItems: 'flex-start', padding: 16, gap:10, flex:1 }}>
+    {set && (
+     <View style={{ alignItems: 'flex-start', padding: 16, gap:10, flex:1 }}>
     <Text style={styles.header}>{set.title}</Text>
     <Text  style={{color: '#666'}}>{set.cards} Cards</Text>
     {set.image && (
@@ -31,8 +36,15 @@ const Page = () => {
         style={{width: '100%', height: 200 }}
         />
     )}
-    </View>}
-        
+    <Text>{set.description}</Text>
+    <Text style={{color: '#666'}}>Created by: {set.creator}</Text>
+    </View>
+    )}
+    <View style={{}}>
+        <TouchableOpacity style={defaultStyleSheet.bottomButton} onPress={onAddToFavorites}>
+        <Text>Add to Favorites</Text>
+        </TouchableOpacity>
+    </View>
     </View>
   )
 }
