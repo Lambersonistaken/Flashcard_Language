f04d3f6a2c383c5042876115add35886ef480aed
Tuğçe Ergün <tugceergun000@gmail.com>
2024-01-02 14:12:03 +0300

    search api connection

--- FlashLanguage/app/(tabs)/search.tsx
+++ FlashLanguage/app/(tabs)/search.tsx
@@ -1,37 +1,44 @@
-import { View, Text } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import {Set, getSets} from "@/data/api"
+
+import { View, Text, StyleSheet, ListRenderItem } from 'react-native'
+import React, {useEffect, useState} from 'react'
+import { Set, getSets } from '@/data/api'
+import { FlatList } from 'react-native-gesture-handler';
+import { defaultStyleSheet } from '@/constants/Styles';
 
 const Page = () => {
-
   const [sets, setSets] = useState<Set[]>([]);
   const [isRefreshing, setIsRefreshing] = useState(false);
 
-
-  useEffect(()=> {
+  useEffect(() => {
     loadSets();
-  }, [])
-
+  }, []);
 
   const loadSets = async () => {
-    try {
-      const data = await getSets();
-      console.log("data : ", data);
-      setSets(data);
-    } catch (error) {
-      console.error("Error loading sets:", error);
-  
-      // Check for specific error properties or types if needed
+    const data = await getSets();
+        console.log("~ file: sets.tsx:11 ~ loadSets ~ data:", data);
+        setSets(data);
+
+        // Check for specific error properties or types if needed
   
       // Handle the error or re-throw it based on your requirements
-    }
-  };
-  
+  }
+
+  const renderSetRow: ListRenderItem<Set> = ({ item }) => {
+    return(
+      <View>
+        <Text>Test</Text>
+      </View>
+    )
+  }
   return (
-    <View>
-      <Text>search page</Text>
+    <View style={defaultStyleSheet.container}>
+     <FlatList data={sets} renderItem={renderSetRow} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+
+})
 
 export default Page
