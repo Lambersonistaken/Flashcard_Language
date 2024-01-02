6bf586b3d45eed3f5ac36ee4d3a315cda131e7ba
Tuğçe Ergün <tugceergun000@gmail.com>
2024-01-02 13:43:34 +0300

    sets api connection

--- FlashLanguage/app/(tabs)/sets.tsx
+++ FlashLanguage/app/(tabs)/sets.tsx
@@ -1,7 +1,20 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { Set, getSets } from '@/data/api'
 
 const Page = () => {
+  const [sets, setSets] = useState<Set[]>([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  useEffect(() => {
+    loadSets();
+  }, []);
+
+  const loadSets = async () => {
+    const data = await getSets();
+        console.log("~ file: sets.tsx:11 ~ loadSets ~ data:", data)
+        setSets(data);
+  }
   return (
     <View>
       <Text>user sets</Text>
