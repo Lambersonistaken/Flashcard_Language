81c3c4018acfe185d50d057f5ba53ba868f0d08e
Tuğçe Ergün <tugceergun000@gmail.com>
2024-01-02 17:28:34 +0300

    sets api connection

--- FlashLanguage/app/(tabs)/sets.tsx
+++ FlashLanguage/app/(tabs)/sets.tsx
@@ -1,10 +1,25 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useEffect} from 'react'
+import { getMySets } from '@/data/api';
 
 const Page = () => {
+  useEffect(() => {
+    loadSets();
+  }, []);
+
+  const loadSets = async () => {
+    const data = await getMySets();
+        console.log("~ file: sets.tsx:14 ~ loadSets ~ data:", data);
+        //setSets(data);
+
+        // Check for specific error properties or types if needed
+  
+      // Handle the error or re-throw it based on your requirements
+  }
+
   return (
     <View>
-      <Text>Sets</Text>
+      <Text>My sets</Text>
     </View>
   )
 }
